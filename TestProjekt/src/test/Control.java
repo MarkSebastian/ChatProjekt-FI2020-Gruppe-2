e2058f8a1433125c17f4e6518a327f903d895bd3
schmidtrobin <schmidtrobin@104WKS05.bsz.edu>
2022-04-29 12:36:32 +0200

    Datenbankauslesen hat funktioniert

--- TestProjekt/src/test/Control.java
+++ TestProjekt/src/test/Control.java
@@ -2,12 +2,15 @@ package test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Control
 {
 	private String verbindungslink;
 	private String sqlBefehl;
+	private Statement stellungsnahme;
 	
 	public Control()
 	{
@@ -17,7 +20,14 @@ public class Control
 		try (Connection verbindung = DriverManager.getConnection(verbindungslink,"","") )
 		{
 			System.out.println("verbindungaufgebaut");
-			System.out.println(verbindung.prepareStatement(sqlBefehl));
+			stellungsnahme = verbindung.createStatement();
+			ResultSet ergebnis = stellungsnahme.executeQuery(sqlBefehl);
+			while (ergebnis.next())
+			{
+				System.out.println(ergebnis.getString("Feld1"));
+				System.out.println(ergebnis.getInt("ID"));
+				
+			}
 		}
 		catch (SQLException e)
 		{

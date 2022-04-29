5be6b027c0ca9b32516f7644ce45bd5376243d09
schmidtrobin <schmidtrobin@104WKS05.bsz.edu>
2022-04-29 11:52:25 +0200

    Erster prototyp datenbankanbindung?

--- TestProjekt/src/test/Control.java
+++ TestProjekt/src/test/Control.java
@@ -1,16 +1,27 @@
 package test;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Control
 {
 	private String verbindungslink;
-	private Connection verbindung;
 	private String sqlBefehl;
+	
 	
 	public Control()
 	{
 		verbindungslink="jdbc:odbc:Testdatenbank";
+		sqlBefehl="Select * from Test;";
 		
+		try (Connection verbindung = DriverManager.getConnection(verbindungslink) )
+		{
+			System.out.println(verbindung.prepareStatement(sqlBefehl));
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
 	}
 }

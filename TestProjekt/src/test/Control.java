076b311784f68709697f35df01f6dec097b431af
schmidtrobin <schmidtrobin@104WKS05.bsz.edu>
2022-04-29 11:43:33 +0200

    anfang daten schmaten

--- TestProjekt/src/test/Control.java
+++ TestProjekt/src/test/Control.java
@@ -1,6 +1,16 @@
 package test;
 
+import java.sql.Connection;
+
 public class Control
 {
+	private String verbindungslink;
+	private Connection verbindung;
+	private String sqlBefehl;
 	
+	public Control()
+	{
+		verbindungslink="jdbc:odbc:Testdatenbank";
+		
+	}
 }

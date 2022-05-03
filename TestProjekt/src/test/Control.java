4a352476bba8dcf068a0bca2f54e63db6abbb32a
schmidtrobin <schmidtrobin@104WKS05.bsz.edu>
2022-05-03 09:05:47 +0200

    Delete funktioniert

--- TestProjekt/src/test/Control.java
+++ TestProjekt/src/test/Control.java
@@ -13,6 +13,7 @@ public class Control
 	private String sqlBefehl;
 	private String sqlAnweisung;
 	private String sqlHochDatum;
+	private String sqlLochen;
 	private Statement stellungsnahme;
 	private int intTest;
 	private String stringTest="Wah";
@@ -24,11 +25,15 @@ public class Control
 		sqlBefehl="Select * from Test;"; //Auslesen
 		sqlAnweisung="Insert into Test (ID, Feld1) values (?,?)";//Insert into
 		sqlHochDatum="Update Test set Feld1=? where Feld1=?";//Update
+		sqlLochen="Delete from Test where Feld1=?";
 		
 		
 		try (Connection verbindung = DriverManager.getConnection(verbindungslink,"","") )
 		{
 			System.out.println("verbindungaufgebaut");
+			PreparedStatement vorbereiteteAussage3 = verbindung.prepareStatement(sqlLochen);
+			vorbereiteteAussage3.setString(1, stringTest);
+			vorbereiteteAussage3.execute();
 			//=======================================================================================Update
 			PreparedStatement vorbereiteteAussage2 = verbindung.prepareStatement(sqlHochDatum);
 			vorbereiteteAussage2.setString(1,stringTest);

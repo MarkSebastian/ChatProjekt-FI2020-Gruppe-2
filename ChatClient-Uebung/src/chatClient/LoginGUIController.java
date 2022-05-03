4d43d38175c5a015d0e8431b6b4ddd9824c2a1d2
RomaSaber <93128365+RomaSaber@users.noreply.github.com>
2022-05-03 10:05:25 +0200

    Zwischenstand pushen

--- ChatClient-Uebung/src/chatClient/LoginGUIController.java
+++ ChatClient-Uebung/src/chatClient/LoginGUIController.java
@@ -1,12 +1,29 @@
 package chatClient;
 
 import javafx.fxml.FXML;
-import javafx.scene.*;
-import com.gluonhq.charm.glisten.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import com.gluonhq.charm.glisten.control.Avatar;
 
 public class LoginGUIController
 {
 	@FXML
 	private Avatar avatarIcon;
-
+	private TextField anmeldenField;
+	private PasswordField passwordField;
+	private Button anmeldenButton;
+	private Label regestrierenLabel;
+	
+	@FXML
+	protected void buttonOnClick()
+	{
+		
+	}
+	
+	protected void labelOnClick()
+	{
+		
+	}
 }

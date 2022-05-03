8823c5dcc12efb31c4ac38ea2e8ee115ad483da6
RomaSaber <93128365+RomaSaber@users.noreply.github.com>
2022-05-03 09:18:41 +0200

    Speichern nicht vergessen Alex

--- ChatClient-Uebung/src/chatClient/LoginGUIController.java
+++ ChatClient-Uebung/src/chatClient/LoginGUIController.java
@@ -2,8 +2,11 @@ package chatClient;
 
 import javafx.fxml.FXML;
 import javafx.scene.*;
+import com.gluonhq.charm.glisten.control.*;
 
 public class LoginGUIController
 {
+	@FXML
+	private Avatar avatarIcon;
 
 }

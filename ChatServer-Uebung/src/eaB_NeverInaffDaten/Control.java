55e889fccc457aeb2a164b7a49a863bb1f928914
schmidtrobin <schmidtrobin@104WKS05.bsz.edu>
2022-05-09 14:01:33 +0200

    kurzerzwischenpush

--- ChatServer-Uebung/src/eaB_NeverInaffDaten/Control.java
+++ ChatServer-Uebung/src/eaB_NeverInaffDaten/Control.java
@@ -2,5 +2,14 @@ package eaB_NeverInaffDaten;
 
 public class Control
 {
-	 
+	private String verbindungslinkLoginserver;
+	private String verbindungslinkDatenbank;
+	private String sqlBefehl;
+	
+	public Control()
+	{
+		verbindungslinkLoginserver="\"jdbc:ucanaccess://Login_DB.accdb\"";
+		verbindungslinkDatenbank="\"jdbc:ucanaccess://Datenkrake.accdb\"";
+		
+	}
 }

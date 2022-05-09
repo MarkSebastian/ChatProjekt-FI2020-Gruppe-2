8c61b72095576c41f72804bead728481e46efd97
schmidtrobin <schmidtrobin@104WKS05.bsz.edu>
2022-05-09 14:56:49 +0200

    lalalalalalalala nein ich werde nicht wahnsinnig

--- ChatServer-Uebung/src/eaB_NeverInaffDaten/Control.java
+++ ChatServer-Uebung/src/eaB_NeverInaffDaten/Control.java
@@ -1,15 +1,54 @@
 package eaB_NeverInaffDaten;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class Control
 {
 	private String verbindungslinkLoginserver;
 	private String verbindungslinkDatenbank;
 	private String sqlBefehl;
+	private Connection verbindungLogin;
+	private Connection verbindungDatenKrake;
+	private SQLBaukasten baukasten;
 	
 	public Control()
 	{
+		baukasten =new SQLBaukasten();
 		verbindungslinkLoginserver="\"jdbc:ucanaccess://src/Login_DB.accdb\"";
 		verbindungslinkDatenbank="\"jdbc:ucanaccess://src/Datenkrake.accdb\"";
 		
+		try 
+		{
+			verbindungLogin = DriverManager.getConnection(verbindungslinkLoginserver,"","");
+			verbindungDatenKrake = DriverManager.getConnection(verbindungslinkDatenbank,"","");
+			
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		
+		
+	}
+	public boolean insert(String bName, String passwort)
+	{
+		boolean erfolg=true;
+		sqlBefehl=baukasten.Insert_LoginDB();
+		try
+		{
+			PreparedStatement vorbereiteteAussage = verbindungLogin.prepareStatement(sqlBefehl);
+			vorbereiteteAussage.setString(1, bName);
+			vorbereiteteAussage.setString(2, passwort);
+			vorbereiteteAussage.executeUpdate();
+		}
+		catch (SQLException e)
+		{
+			erfolg=false;
+		}
+		
+		return erfolg;
 	}
 }

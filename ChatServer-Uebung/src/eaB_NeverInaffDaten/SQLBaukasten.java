64959766917250c344e9142e00dd213908d5cd8d
kellertobias <kellertobias@104WKS06.bsz.edu>
2022-05-16 13:20:47 +0200

    push?

--- ChatServer-Uebung/src/eaB_NeverInaffDaten/SQLBaukasten.java
+++ ChatServer-Uebung/src/eaB_NeverInaffDaten/SQLBaukasten.java
@@ -208,7 +208,7 @@ public class SQLBaukasten
 	protected String select_nachricht_in_chatroom()
 	{
 		return select+"cl."+benutzername+komma+"n."+timestamp+komma+"n."+inhalt+from+nachricht+"n "+komma+chatroom+"ch "+where+"n."+chatroom_id+ist+"ch."+id;
-	}
+	} 
 	
 	
 }

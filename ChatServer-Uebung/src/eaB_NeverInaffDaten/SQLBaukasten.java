7ac893071e41bb5e5d53476189fc80190328679f
kellertobias <kellertobias@104WKS06.bsz.edu>
2022-05-17 09:18:47 +0200

    Commentary Commentary

--- ChatServer-Uebung/src/eaB_NeverInaffDaten/SQLBaukasten.java
+++ ChatServer-Uebung/src/eaB_NeverInaffDaten/SQLBaukasten.java
@@ -49,6 +49,7 @@ public class SQLBaukasten
 	
 	public SQLBaukasten()
 	{
+		//initialisieren
 		chatroom = "Chatroom ";
 		client = "Client ";
 		clientChatroom_ZT = "Client/Chatroom_ZT ";
@@ -96,27 +97,32 @@ public class SQLBaukasten
 	//deletes
 	protected String delete_loginDaten()
 	{
+		//Löscht Zeile mit LoginDaten aus login_Daten
 		return delete+from+login_Daten+where+"Benutzername"+ist+fragezeichen;
 	}
 	
 	//inserts
 	protected String insert_LoginDB()
 	{
+		//induziert Zeile mit logindaten in login_Daten
 		return insert+into+login_Daten+klammerAuf+benutzername+komma+passwort+klammerZu+values+klammerAuf+fragezeichen+komma+fragezeichen+klammerZu;
 	}
 	
 	protected String insert_Client()
 	{
+		//lötet Zeile mit clientdaten auf die Metallplatte
 		return insert+into+client+klammerAuf+benutzername+klammerZu+values+klammerAuf+fragezeichen+klammerZu; 
 	}
 	
 	protected String insert_Loginliste()
 	{
+		//schreibt in die liste der Logins eine neuen login
 		return insert+into+loginliste+klammerAuf+timestamp_beginn+komma+ip+klammerZu+values+klammerAuf+fragezeichen+komma+fragezeichen+klammerZu; 
 	}
 	
 	protected String insert_LoginClientZT() 
 	{
+		//Verbindet Login und Client->Zuordnungstabelle
 		return insert+into+loginClient_ZT+klammerAuf+loginliste_id+komma+client_id+klammerZu+values+klammerAuf+
 
 				klammerAuf+select_Client_id()+klammerZu+komma+
@@ -125,11 +131,13 @@ public class SQLBaukasten
 	
 	protected String insert_Chatroom()
 	{
+		//gewährt einen eintrag für einen chatroom
 		return insert+into+chatroom+klammerAuf+chatroomname+komma+hashcode+klammerZu+values+fragezeichen;
 	}
 	
 	protected String insert_ClientChatroomZT()
 	{
+		//stellt Verbindung zwischen Chatroom und Client her. verwendet für hinzufügen von cliets zu chatrooms
 		return insert+into+clientChatroom_ZT+klammerAuf+client_id+komma+chatroom_id+klammerZu+values+klammerAuf+
 				klammerAuf+select_Client_id()+klammerZu+komma+
 				select_Chatroom_nach_hashcode()+klammerZu; 
@@ -137,6 +145,7 @@ public class SQLBaukasten
 	
 	protected String insert_Nachricht()
 	{
+		//Nachricht einfügen
 		return insert+into+nachricht+klammerAuf+inhalt+komma+timestamp+komma+client_id+komma+chatroom_id+klammerZu+
 				values+klammerAuf+fragezeichen+komma+fragezeichen+komma+
 				select_Client_id()+komma+
@@ -146,67 +155,74 @@ public class SQLBaukasten
 	//updates
 	protected String update_Benutzername()
 	{
+		//benutzername (nicht Accname) wird aktualisiert
 		return update+client+set+benutzername+ist+fragezeichen+where+benutzername+ist+fragezeichen;
 	}
 	
 	protected String update_timestamp()
 	{
+		//bei Ausloggen wird der momentane login mit end-timestamp versehen
 		return update+loginliste+set+timestamp_ende+ist+fragezeichen+where+id+ist;
 	}
 	
 	//selects
 	protected String select_Client_id()
 	{
+		//schnappt sich die client id aus dem accname
 		return select+id+from+client+where+accountname+ist+fragezeichen;
 	}
 	
 	protected String select_Loginlisten_id()
 	{
+		//zieht sich die id aus loginliste mit der angefragten ip und angefragten timestamp_beginn
 		return select+id+from+loginliste+where+timestamp_beginn+ist+fragezeichen+and+ip+ist+fragezeichen ;
 	}
 	
 	protected String select_Chatroom_nach_hashcode()
 	{
+		//gibt die id des chatrooms mit bekanntem hashcode
 		return select+id+from+chatroom+where+hashcode+ist+fragezeichen;
 	}
 	
 	protected String select_Chatroomname_nach_hashcode()
 	{
+		//holt den namen des chatrooms mit brkanntem hashcode
 		return select+chatroomname+from+chatroom+where+hashcode+ist+fragezeichen;
 	}
 	
 	protected String select_hashcode()
 	{
+		//beantwortet die frage, ob hashcode bereits vorhanden
 		return select+hashcode+from+chatroom+where+hashcode+ist+fragezeichen;
 	}
 	
 	protected String select_Loginname()
 	{
+		//abfrage zur überprüfung ob benutzername frei ist
 		return select+benutzername+from+login_Daten+where+benutzername+ist+fragezeichen;
 	}
 	
 	protected String select_passwort()
 	{
+		//schnabuliert sich das passwort zum benutzer
 		return select+passwort+from+login_Daten+where+benutzername+ist+fragezeichen;
-	}
-	
-	protected String select_chatroomname_nach_hashcode()
-	{
-		return select+chatroomname+from+chatroom+where+hashcode+ist+fragezeichen;
 	}
 	
 	protected String select_benutzername_nach_Client_id()
 	{
+		//gönnt sich den accountnamen für bekannte id aus client
 		return select+accountname+from+client+where+id+ist+fragezeichen;
 	}
 	
 	protected String select_chatroomnamen_by_client()
 	{
+		//schenkt dem server die ergebnisse aus chatrooms für bekannten accnamen eines clients
 		return select+chatroomname+from+chatroom+where+id+ist+klammerAuf+select+chatroom_id+from+clientChatroom_ZT+where+client_id+ist+klammerAuf+select_Client_id()+klammerZu+klammerZu;
 	}
 	
 	protected String select_nachricht_in_chatroom()
 	{
+		//jöötet die nachrichten aus bekannten chatroom aus
 		return select+"cl."+benutzername+komma+"n."+timestamp+komma+"n."+inhalt+from+nachricht+"n "+komma+client+"cl"+komma+chatroom+"ch "+where+"n."+chatroom_id+ist+"ch."+id+and+"ch."+hashcode+ist+fragezeichen;
 	} 
 	 

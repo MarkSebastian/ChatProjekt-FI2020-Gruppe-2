61c8809cf90e92e498067169f42cfa4075645c6e
MuellerJannes <MuellerJannes@104WKS04.bsz.edu>
2022-05-24 09:17:51 +0200

    Am server angefangen zu arbeiten

--- ChatServer-Uebung/src/chatServer/MainServer.java
+++ ChatServer-Uebung/src/chatServer/MainServer.java
@@ -1,9 +1,26 @@
 package chatServer;
 
-public class MainServer
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainServer extends Application
 {
 	public static void main(String[] args)
 	{
 		new Control();
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage stage) throws Exception
+	{
+		Parent serverParent = FXMLLoader.load(getClass().getResource("Server.fxml"));
+		Scene scene = new Scene(serverParent);
+		stage.setTitle("Server");
+		stage.setScene(scene);
+		stage.show();
 	}
 }
